be2ecde378c046f787c9321b7a824d2a5e62b88c
vickymurugasen <vickymurugasen14@gmail.com>
2022-05-11 11:35:32 +0530

    today

---
+++ exfloat.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+class exfloat
+{
+public static void main (String args[])
+{
+Scanner sc=new Scanner (System.in);
+System.out.println("Enter float value");
+float f=sc.nextFloat();
+float g=sc.nextFloat();
+System.out.println("Enter char");
+char c=sc.next().charAt(0);
+switch(c)
+{ 
+case '+':      
+System.out.println ("Add values : "+(f+g));
+break;
+case '-':
+System.out.println ("sub values : "+(f-g));
+break;
+case '*':
+System.out.println ("Mul values : "+(f*g));
+break;
+case '/':
+System.out.println("Div values : "+(f/g));
+break;
+}
+}
+}
+
+/*
+
+
+float a
+float b
+
+cr= /
+*
+-
++
+
+
+8 2 /  4 
+
+8 2 *  16
+*/
